57d62f56042f1bffaacf450e03e6beaff356cd9f
symoon94 <msy0128@gmail.com>
2019-03-22 15:54:05 -0700

    add comments

--- Rilakkuma.py
+++ Rilakkuma.py
@@ -1,3 +1,8 @@
+###
+### Author     : Sooyoung Moon
+### Description: This program draws a Rilakkuma with eye blinking.
+###
+
 from graphics import graphics
 import random
 
@@ -6,17 +11,21 @@ def main():
     i = 0
     while True:
         gui.clear()
+        ## background
         gui.rectangle(0, 0, 400, 650, 'gray12')
+        ## ears
         gui.ellipse(70, 400, 220, 200, 'orange')
         gui.ellipse(30, 400, 200, 130, 'yellow')
         gui.ellipse(330, 400, 220, 200, 'orange')
         gui.ellipse(370, 400, 200, 130, 'yellow')
+        ## face
         gui.ellipse(200, 600, 600, 500, 'orange')
-        gui.ellipse(200, 600, 600, 500, 'orange')
+        ## nose and mouth
         gui.ellipse(200, 620, 180, 150, 'white')
         gui.ellipse(200, 600, 40, 40, 'black')
         gui.line(200, 615, 160, 640, 'black', 8)
         gui.line(200, 615, 240, 640, 'black', 8)
+        ## eyes
         if i <= 30:
             gui.ellipse(70, 550, 45, 15+i, 'black')
             gui.ellipse(330, 550, 45, 15+i, 'black')
@@ -24,8 +33,8 @@ def main():
             gui.ellipse(70, 550, 45, 45-(i-30), 'black')
             gui.ellipse(330, 550, 45, 45-(i-30), 'black')
         i = i%60
-        i += 10
-        gui.update_frame(2)
+        i += 2
+        gui.update_frame(10)
 
 main()
 
